ee3ce73d78ee35bf06dcdca48a21cddc2bb57338
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-01-05 11:39:20 +0530

    read encapsulation

---
+++ OOPS/Encapsulation/Encapsulation-Pratice-Question/Q2/JavaApplication.java
@@ -0,0 +1,7 @@
+class JavaApplication {
+	public static void execute(){
+		ProductDetails pp=new ProductDetails(101,"Table");
+		System.out.println(pp.getProductId());;
+		System.out.println(pp.getProductName());;
+	}
+}
---
+++ OOPS/Encapsulation/Encapsulation-Pratice-Question/Q2/ProductDetails.java
@@ -0,0 +1,28 @@
+// Q3. 
+// Read-Only Encapsulation
+
+// Class Name: ProductDetails
+// Private Variables:
+// int productId
+// String productName
+// Methods:
+// public Product(int productId, String productName)
+// public int getProductId()
+// public String getProductName()
+// Condition:
+// No setter methods allowed
+
+class ProductDetails {
+	private int productId;
+	private String productName;
+	public ProductDetails(int productId,String productName){
+		this.productId=productId;
+		this.productName=productName;
+	}
+	public int getProductId(){
+		return productId;
+	}
+	public String getProductName(){
+		return productName;
+	}
+}
