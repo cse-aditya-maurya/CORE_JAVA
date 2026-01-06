e217866b8a07f6f6063594a1f6bf89d78f585964
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-01-06 14:56:12 +0530

    method overloading

---
+++ OOPS/Polymorphism/Compile-time polymorphism/Q2/Instagram.java
@@ -0,0 +1,31 @@
+class Instagram {
+	public String username;
+	public String password;
+	public String mailId;
+	public String captch;
+	public long contactNubmer;
+	public int otp;
+	public void login(String username,String password){
+		this.username=username;
+		this.password=password;
+		System.out.println("Log in successfull!!");
+	}
+	public void login(String username,String password,String captch){
+		this.username=username;
+		this.password=password;
+		this.captch=captch;
+		System.out.println("Log in successfull!!");
+	}
+	public void login(long contactNubmer,int otp){
+		this.contactNubmer=contactNubmer;
+		this.otp=otp;
+		System.out.println("Log in successfull!!");
+	}
+	public void login(String mailId,int otp)
+	{
+		this.mailId=mailId;
+		this.otp=otp;
+		System.out.println("Log in successfull!!");
+
+	}
+}
---
+++ OOPS/Polymorphism/Compile-time polymorphism/Q2/JavaApplication.java
@@ -0,0 +1,11 @@
+class JavaApplication {
+	public static void execute(){
+		Instagram ss=new Instagram();
+		ss.login("Mayank143","4567");
+		ss.login("Rohan221","6789","a1b2c3");
+		ss.login("mayank@123",2443);
+		ss.login(9876543211l,6789);
+		
+
+	}
+} 
