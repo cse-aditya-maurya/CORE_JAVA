85654b1667cfea3ce19986539667c3afaf3d41e7
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-01-06 10:25:20 +0530

    Polymorphism

---
+++ OOPS/Polymorphism/Compile-time polymorphism/Question/JavaApplication.java
@@ -0,0 +1,9 @@
+class JavaApplication {
+	public static void execute(){
+		System.out.println(BasicCalculator.addition(5,8));
+		System.out.println(BasicCalculator.addition(20,30,40));
+		System.out.println(BasicCalculator.addition(6.8,9.0));
+		System.out.println(BasicCalculator.addition(6.8,9.0,5.5));
+
+	}
+}
