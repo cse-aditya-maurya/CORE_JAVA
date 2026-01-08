ace20b5800d46a18f37510971069c29e967e3138
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-01-08 10:11:57 +0530

    interface

---
+++ OOPS/Abstraction/InterFace/Q3/Concrete.java
@@ -0,0 +1,10 @@
+class Concrete implements Interface {
+    @Override
+    public  void nsm1(){
+	System.out.println("non static nsm1() of class Concrete");
+	}
+	@Override
+	public void nsm2(){
+		System.out.println("non static nsm2() abstract of concrete class ");
+	}
+}
---
+++ OOPS/Abstraction/InterFace/Q3/JavaApplication.java
@@ -0,0 +1,7 @@
+class JavaApplication{
+	public static void execute(){
+		Concrete cc=new Concrete();
+		cc.nsm1();
+		cc.nsm2();
+	}
+}
